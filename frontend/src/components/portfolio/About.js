5c15153cfc4262d52ed1c2075d76c66159849f48
Jason <jgodmere808@gmail.com>
2020-09-24 03:41:02 -0500

    Finished skeleton of About-pie-chart. Displays pie chart with 3 slices (front-end, back-end, mobile) and the libraries/frameworks that I am familiar with that relate to each slice: no languages listed except for mobile.

--- frontend/src/components/portfolio/About.js
+++ frontend/src/components/portfolio/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Pie} from 'react-chartjs-2';
 import './Portfolio.css';
 
 // images
@@ -12,7 +13,7 @@ export default class About extends Component {
   render() {
     return (
       <div className="Port-container">
-        <h1 className="Port-header--lg">About</h1>
+        <h1 className="Port-header--lg">ABOUT</h1>
 
         <div className="About-header">
           <div>
@@ -44,7 +45,69 @@ export default class About extends Component {
             </p>
           </div>
         </div>
+
+        <div class="About-chart-container">
+          <div>
+            <h2 class="Port-header--sm Port-header--list">Back-End</h2>
+            <ul class="Port--list">
+              <li>Django</li>
+              <li>ORM</li>
+              <li>SQL</li>
+              <li>Wordpress</li>
+              <li>Flask</li>
+            </ul>
+          </div>
+          <div>
+            <Pie class="About-pie-chart"
+              data={chartData}
+              options={{
+                legend: {
+                  labels: {
+                    fontSize: 14
+                  }
+                },
+                rotation: 0.45 * Math.PI,
+                maintainAspectRatio: false
+              }}
+            />
+          </div>
+          <div>
+            <h2 class="Port-header--sm Port-header--list">Front-End</h2>
+            <ul class="Port--list">
+              <li>React</li>
+              <li>D3</li>
+              <li>Chart.js</li>
+              <li>jQuery</li>
+            </ul>
+            <h2 class="Port-header--sm Port-header--list">Mobile</h2>
+            <ul class="Port--list">
+              <li>Swift-UI</li>
+              <li>Swift</li>
+              <li>Kotlin</li>
+            </ul>
+          </div>
+        </div>
+
       </div>
     )
   }
+}
+
+const chartData = {
+  labels: ['Back-End', 'Front-End', 'Mobile'],
+  datasets: [{
+    label: '# of Votes',
+    data: [45, 45, 10],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)'
+    ],
+    borderWidth: 1
+  }]
 }
--- frontend/src/components/portfolio/Projects.js
+++ frontend/src/components/portfolio/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Projects() {
   return (
     <div className="Port-container Projects-container">
-      <h1 className="Port-header--lg">Projects</h1>
+      <h1 className="Port-header--lg">PROJECTS</h1>
     </div>
   )
 }
